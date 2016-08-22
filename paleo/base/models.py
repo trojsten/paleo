e11a71c1fce5fc5914609ab61496321610984ca6
Marián Horňák <marian.sysel.hornak@gmail.com>
2016-08-22 16:47:50 +0200

    backup

--- paleo/base/models.py
+++ paleo/base/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from django.config import settings
+
+
+class Service(models.Model):
+    slug = models.SlugField(max_length=16, primary_key=True)
+
+
+class User(models.Model):
+    auth_user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        primary_key=True,
+        related_name='paleo_user'
+    )
+
+    allowed_services = models.ManyToManyField(Service, related_name='users')
+
+
+class Team(models.Model):
+    members = models.ManyToManyField(User, related_name='teams')
+
+
+class Group(models.Model):
+    teams = models.ManyToManyField(Team, related_name='groups')
+
+    allowed_services = models.ManyToManyField(Service, related_name='groups')
